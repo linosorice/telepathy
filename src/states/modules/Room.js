c10a8463a7b291df03b89f4a804f6fcf8e995818
Nicola Squartini <tensor5@gmail.com>
2018-01-27 04:59:51 +0100

    Double door size

--- src/states/modules/Room.js
+++ src/states/modules/Room.js
@@ -22,7 +22,7 @@ export default class Room {
     const {x, y} = this.options
     for (var i = 0; i < ROOM_COLS; i++) {
       for (var j = 0; j < ROOM_ROWS; j++) {
-        if (i === 0 && j === (ROOM_ROWS - 1) / 2) {  // north door
+        if (i === 0 && (j === ROOM_ROWS / 2 || j === ROOM_ROWS / 2 - 1)) {  // north door
           if (map[x][y].u) {
             this.game.add.existing(
               new Ground({
@@ -38,7 +38,7 @@ export default class Room {
           }
         }
 
-        if (i === (ROOM_COLS - 1) / 2 && j === ROOM_ROWS - 1) {  // east door
+        if ((i === ROOM_COLS / 2 || i === ROOM_COLS / 2 - 1) && j === ROOM_ROWS - 1) {  // east door
           if (map[x][y].r) {
             this.game.add.existing(
               new Ground({
@@ -47,14 +47,14 @@ export default class Room {
                   ROOM_SIZE * (x - (BATTLEFIELD_COLS - 1) / 2),
                 x: TILE_SIZE_SPACED * (j - (ROOM_ROWS - 1) / 2) + this.game.world.centerX +
                   ROOM_SIZE * (y - (BATTLEFIELD_ROWS - 1) / 2),
-                north: true
+                north: i === ROOM_COLS / 2 ? false : true
               })
             )
             continue
           }
         }
 
-        if (i === (ROOM_COLS - 3) / 2 && j === ROOM_ROWS - 1) {  // east wall above door
+        if (i === ROOM_COLS / 2 - 2 && j === ROOM_ROWS - 1) {  // east wall above door
           if (map[x][y].r) {
             this.game.add.existing(
               new Wall({
@@ -70,7 +70,7 @@ export default class Room {
           }
         }
 
-        if (i === ROOM_COLS - 1 && j === (ROOM_ROWS - 1) / 2) {  // south door
+        if (i === ROOM_COLS - 1 && (j === ROOM_ROWS / 2 - 1 || j === ROOM_ROWS / 2)) {  // south door
           if (map[x][y].d) {
             this.game.add.existing(
               new Ground({
@@ -86,7 +86,7 @@ export default class Room {
           }
         }
 
-        if (i === (ROOM_COLS - 1) / 2 && j === 0) {  // west door
+        if ((i === ROOM_COLS / 2 - 1 || i === ROOM_COLS / 2) && j === 0) {  // west door
           if (map[x][y].l) {
             this.game.add.existing(
               new Ground({
@@ -95,14 +95,14 @@ export default class Room {
                   ROOM_SIZE * (x - (BATTLEFIELD_COLS - 1) / 2),
                 x: TILE_SIZE_SPACED * (j - (ROOM_ROWS - 1) / 2) + this.game.world.centerX +
                   ROOM_SIZE * (y - (BATTLEFIELD_ROWS - 1) / 2),
-                north: true
+                north: i === ROOM_COLS / 2 ? false : true
               })
             )
             continue
           }
         }
 
-        if (i === (ROOM_COLS - 3) / 2 && j === 0) {  // west wall above door
+        if (i === ROOM_COLS / 2 - 2 && j === 0) {  // west wall above door
           if (map[x][y].l) {
             this.game.add.existing(
               new Wall({
@@ -139,7 +139,7 @@ export default class Room {
               ROOM_SIZE * (x - (BATTLEFIELD_COLS - 1) / 2),
             x: TILE_SIZE_SPACED * (j - (ROOM_ROWS - 1) / 2) + this.game.world.centerX +
               ROOM_SIZE * (y - (BATTLEFIELD_ROWS - 1) / 2),
-            north: i === 1 && (j !== (ROOM_COLS - 1) / 2 || map[x][y].u === false)
+            north: i === 1 && ((j !== ROOM_COLS / 2 - 1 && j !== ROOM_COLS / 2) || map[x][y].u === false)
           })
         )
       }
