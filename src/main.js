20e4aa0cc1623a46c3bb8479bed65ded9d3afaa5
Pasqualino Sorice <lino.sorice@gmail.com>
2018-01-28 09:38:38 +0100

    fix window size

--- src/main.js
+++ src/main.js
@@ -12,15 +12,15 @@ import config from './config'
 
 import io from 'socket.io-client'
 
-const socket = io('http://localhost:8000', {path: '/gamews', forceNew: true})
+const socket = io('http://localhost:3456', {path: '/gamews', forceNew: true})
 
 class Game extends Phaser.Game {
   constructor () {
-    const docElement = document.documentElement
-    const width = docElement.clientWidth > config.gameWidth ? config.gameWidth : docElement.clientWidth
-    const height = docElement.clientHeight > config.gameHeight ? config.gameHeight : docElement.clientHeight
+    // const docElement = document.documentElement
+    // const width = docElement.clientWidth > config.gameWidth ? config.gameWidth : docElement.clientWidth
+    // const height = docElement.clientHeight > config.gameHeight ? config.gameHeight : docElement.clientHeight
 
-    super(width, height, Phaser.CANVAS, 'content', null)
+    super(config.gameWidt, config.gameHeight, Phaser.AUTO, 'content', null)
 
     this.state.add('Boot', BootState, false)
     this.state.add('Splash', SplashState, false)
