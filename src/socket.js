a8b1402cab51347032ce240890e6547326e62aa5
Federico Squartini <federico.squartini@gmail.com>
2018-01-28 13:57:36 +0100

    Remove hardcoded socket.io address

--- src/socket.js
+++ src/socket.js
@@ -1,6 +1,6 @@
 import io from 'socket.io-client'
 
-const socket = io('http://192.168.139.81:8000', {path: '/gamews', forceNew: true})
+const socket = io({path: '/gamews', forceNew: true})
 
 export default socket
 
